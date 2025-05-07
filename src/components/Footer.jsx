d7137bbe06710ea4ab0f4529863a62e6dd766ec8
Raed Nimer <raed.nimer7@gmail.com>
2025-05-07 18:46:45 +0000

    feat: Add Footer

---
+++ src/components/Footer.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <>
+      <footer className="mb-0 text-center bg-dark text-white">
+        <div className="d-flex align-items-center justify-content-center pb-5 pt-5">
+          <div className="col-md-6">
+            <p className="mb-3 mb-md-0">&copy; 2025 Powerhouse. All rights reserved. {" "}
+              <a  href="" className="text-decoration-underline text-dark fs-5" target="_blank" rel="noreferrer"></a>
+            </p>    
+          </div>
+        </div>
+      </footer>
+    </>
+  );
+};
+
+export default Footer;
