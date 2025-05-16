79e01a1c178faf404922b65666b0fa1754617fb9
Raed Nimer <raed.nimer7@gmail.com>
2025-05-16 18:02:48 +0000

    feat: Update main.jsx

---
+++ src/main.jsx
@@ -0,0 +1,68 @@
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import "../node_modules/font-awesome/css/font-awesome.min.css";
+import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
+import { BrowserRouter, Routes, Route } from "react-router";
+import { Provider } from "react-redux";
+import store from "./redux/store";
+import ScrollToTop from "./components/ScrollToTop";
+import { Toaster } from "react-hot-toast";
+import { AuthProvider } from "./context/AuthContext"; 
+import PrivateRoute from  "./components/PrivateRoute";
+import {
+  Home,
+  Product,
+  Products,
+  AboutPage,
+  ContactPage,
+  Cart,
+  Login,
+  Register,
+  Checkout,
+  PageNotFound,
+} from "./pages";
+import Profile from "./pages/Profile";
+
+createRoot(document.getElementById("root")).render(
+  <StrictMode>
+    <BrowserRouter>
+      <ScrollToTop>
+      <AuthProvider>
+        <Provider store={store}>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/product" element={<Products />} />
+            <Route path="/product/:id" element={<Product />} />
+            <Route path="/about" element={<AboutPage />} />
+            <Route path="/contact" element={<ContactPage />} />
+            <Route path="/cart" element={<Cart />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/register" element={<Register />} />
+            <Route
+                path="/checkout"
+                element={
+                  <PrivateRoute>
+                    <Checkout />
+                  </PrivateRoute>
+                }
+              />
+
+            <Route
+                path="/profile"
+                element={
+                  
+                    <Profile />
+                 
+                }
+              />
+            <Route path="*" element={<PageNotFound />} />
+            <Route path="/product/*" element={<PageNotFound />} />
+          </Routes>
+        </Provider>
+        </AuthProvider>
+      </ScrollToTop>
+      <Toaster />
+    </BrowserRouter>
+  </StrictMode>
+);
