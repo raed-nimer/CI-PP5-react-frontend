63c8bcb657eb8cb0a0a2a5d3d95490516ea6c17e
Raed Nimer <raed.nimer7@gmail.com>
2025-05-16 18:01:13 +0000

    fix: Update Product page

--- src/pages/Product.jsx
+++ src/pages/Product.jsx
@@ -24,7 +24,7 @@ const Product = () => {
     const getProduct = async () => {
       setLoading(true);
       setLoading2(true);
-      const response = await fetch(`https://fakestoreapi.com/products/${id}`);
+      const response = await fetch(`${import.meta.env.VITE_APP_SERVER_URL}/api/products/${id}`);
       const data = await response.json();
       setProduct(data);
       setLoading(false);
