86a0cb190b3a2da07c7bf1bda908b1b3d141eff2
Raed Nimer <141240830+raed-nimer@users.noreply.github.com>
2025-05-23 14:37:53 +0500

    feat: Create CartContext.jsx

---
+++ src/context/CartContext.jsx
@@ -0,0 +1,39 @@
+// src/context/CartContext.js
+import { createContext, useContext, useEffect, useState } from "react";
+
+const CartContext = createContext();
+
+export const useCart = () => useContext(CartContext);
+
+export const CartProvider = ({ children }) => {
+  const [cartCount, setCartCount] = useState(0);
+
+  const fetchCartCount = async () => {
+    const token = localStorage.getItem("accessToken");
+    try {
+      const response = await fetch(`${import.meta.env.VITE_APP_SERVER_URL}/api/cart`, {
+        headers: { Authorization: `Bearer ${token}` },
+      });
+
+      if (!response.ok) throw new Error("Fetch failed");
+
+      const cartItems = await response.json();
+      const totalQuantity = cartItems.reduce((sum, item) => sum + (item.quantity || 0), 0);
+
+      setCartCount(totalQuantity);
+      localStorage.setItem("cartCount", totalQuantity);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  useEffect(() => {
+    fetchCartCount();
+  }, []);
+
+  return (
+    <CartContext.Provider value={{ cartCount, setCartCount, fetchCartCount }}>
+      {children}
+    </CartContext.Provider>
+  );
+};
