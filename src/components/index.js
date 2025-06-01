ae6588f6310e30317be95869ab4bff2eebece2df
Raed Nimer <141240830+raed-nimer@users.noreply.github.com>
2025-06-01 11:29:09 +0500

    feat: Add import to index.js

--- src/components/index.js
+++ src/components/index.js
@@ -1,5 +1,6 @@
 export { default as Navbar } from './Navbar';
 export { default as Main } from './main';
 export { default as Product } from './Products';
+export { default as ShowCheckout } from "./ShowCheckout";
 export { default as Footer } from './Footer';
 export { default as PrivateRoute } from "./PrivateRoute";
